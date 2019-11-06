f77800e5f0052795c7cb87ff7645f5b3a1449979
League Student <league@iMac-20.attlocal.net>
2019-11-05 19:59:28 -0800

    wait

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,10 +1,13 @@
 package _03_IntroToStacks;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo {
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -17,7 +20,9 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	public static void main(String[] args) {
+
+
+	void start(){
 		JFrame frame  = new JFrame();
 		JPanel panel  = new JPanel();
 		JLabel label  = new JLabel();
@@ -25,7 +30,27 @@ public class _02_TextUndoRedo {
 		panel.add(label);
 		frame.pack();
 		frame.setVisible(true);
+		frame.addKeyListener(this);
 	}
-	
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		int keycode = e.getKeyCode();
+		System.out.println(keycode);
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
