0c8f73eb3fa96887aeee3c6adc6deb2400cc1818
League Student <league@iMac-10.attlocal.net>
2019-11-19 19:56:09 -0800

    tmb :sunglasses:

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -11,33 +11,34 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class _02_TextUndoRedo implements KeyListener {
-	/* 
+	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 * Every time a key is pressed, add that character to the JLabel. It should look
+	 * like a basic text editor.
 	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
+	 * Make it so that every time the BACKSPACE key is pressed, the last character
+	 * is erased from the JLabel. Save that deleted character onto a Stack of
+	 * Characters.
 	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
+	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
+	 * the top Character is popped off the Stack and added back to the JLabel.
 	 * 
-	 * */
+	 */
 
-	
 	Stack<Character> labelName = new Stack<Character>();
 	ArrayList<Character> ar;
-	JFrame frame  = new JFrame();
-	JPanel panel  = new JPanel();
-	JLabel label  = new JLabel();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
 	String str = "";
 
-	void start(char labelName){
-		
+	void start(char labelName) {
+
 		frame.add(panel);
 		panel.add(label);
-		panel.setPreferredSize(new Dimension (600,600));
-	
+		panel.setPreferredSize(new Dimension(600, 600));
+
 		label.setText(String.valueOf(labelName));
 		frame.pack();
 		frame.setVisible(true);
@@ -52,27 +53,37 @@ public class _02_TextUndoRedo implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e1) {
 		// TODO Auto-generated method stub
-		
+
+		int keyCode = e1.getKeyCode();
 		char keyChar = e1.getKeyChar();
 		System.out.println(keyChar);
-		str+=keyChar;
+		System.out.println(keyCode);
+		if (keyCode == 8) {
+			labelName.push(str.charAt(str.length() - 1));
+			str = str.substring(0, str.length() - 1);
+		}else if (keyCode == 127) {
+
+			str+=labelName.pop();
+		
+		}else {
+			str += keyChar;	
+		}
+		
 		System.out.println(str);
 		label.setText(str);
 		frame.pack();
-		//labelName.push(keyChar);
-		//int sc = labelName.size();
-		//for(int i = 0; i < sc; i++){
-		//	label.setText(String.valueOf(labelName.pop()));
-		//}
+		// labelName.push(keyChar);
+		// int sc = labelName.size();
+		// for(int i = 0; i < sc; i++){
+		// label.setText(String.valueOf(labelName.pop()));
+		// }
 
-		
 	}
-	
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -8,7 +8,7 @@ import java.util.Stack;
 import org.junit.Test;
 
 public class _03_TestMatchingBrackets {
-
+	Stack<String> brackets = new Stack<String>();
 	@Test
 	public void testMatchingBrackets() {
 		assertTrue(doBracketsMatch("{}"));
@@ -20,6 +20,15 @@ public class _03_TestMatchingBrackets {
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
+		for (int i = 0; i < b.length(); i++) {
+			if(b.charAt(i) == '{') {
+				brackets.push("{");
+			}	
+		}
+	
+		if(b.contentEquals("{")){
+			brackets.push("{");
+		}
 		
 		return false;
 	}
