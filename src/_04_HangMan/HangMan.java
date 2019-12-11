fb5c7cc9bef1ef4dab810c9b121fadd9202b7ac0
League Student <league@iMac-11.attlocal.net>
2019-12-10 19:44:18 -0800

    yeet

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -8,27 +8,41 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class HangMan{
-void start() {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	Stack<String> words = new Stack<String>();
+	String current;
+	int chars;
+void start() {
+	
 	frame.add(panel);
 	panel.add(label);
+	frame.setVisible(true);
 	int numOfWords = Integer.parseInt(JOptionPane.showInputDialog("Yeety"));
-	Stack<String> words = new Stack<String>();
+	
 	for (int i = 0; i < numOfWords;) {
 		String ran = Utilities.readRandomLineFromFile("dictionary.txt");
 		if (words.contains(ran)==false) {
 			words.push(ran);
 			i++;
-		}
+		}	 
+	}
+
+		
+	
+	for (int i = 0; i < current.length(); i++) {
+		label.setText(current);
 	}
 	
-	for (int i = 0; i < numOfWords; i++) {
-		System.out.println(words.pop());
-	}
+
 	
 }
 	
-	
+public void getNextWord() {
+		if(!words.isEmpty()) {
+			current = words.pop();
+		}
+
+	}
 }
